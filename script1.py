4c4af6e846d96be19c33a8a7795b92ec5254d020
haval95 <havalrezan22@gmail.com>
2023-04-20 14:57:14 +0300

    creating a simple web application template with flask

--- script1.py
+++ script1.py
@@ -1,13 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return "webiste content"
-@app.route('/contact')
-def contact():
-    return "webiste contact page"
 
-if __name__ == '__main__':
+@app.route("/")
+def home():
+    return render_template("home.html", active_page='home')
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html", active_page='about')
+
+
+if __name__ == "__main__":
     app.run(debug=True)
